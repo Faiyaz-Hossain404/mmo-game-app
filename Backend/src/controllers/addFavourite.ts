bb88b6cad475c99ed27dedc2dfee55e0dc15a521
Fiayz <sifathhossain115@gmail.com>
2025-07-16 22:02:04 +0600

    Controller on going

--- Backend/src/controllers/addFavourite.ts
+++ Backend/src/controllers/addFavourite.ts
@@ -1,7 +1,18 @@
+import { time } from "console";
 import { Request, Response, NextFunction } from "express";
+import Favourite from "../Models/Favourite";
 
 export const addFavourite = async (req: Request, res: Response, next: NextFunction) => {
     try{
-        
+        const {id, name, timestamp} = req.body;
+
+        if(!id || !name || !timestamp){
+            return res.status(400).json({error: "Missing required fields."})
+        }
+
+        const exists = await Favourite.findByPk(id);
+        if(exists)[
+            return res.status()
+        ]
     }
 }
