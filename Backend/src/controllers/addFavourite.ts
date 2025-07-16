a8740f0d3f340b57bf66006c1be54b06fe79a44f
Fiayz <sifathhossain115@gmail.com>
2025-07-16 22:32:08 +0600

    'Favourite' controller completed get, add and delete implemented in one file

--- Backend/src/controllers/addFavourite.ts
+++ Backend/src/controllers/addFavourite.ts
@@ -1,18 +1,57 @@
-import { time } from "console";
 import { Request, Response, NextFunction } from "express";
 import Favourite from "../Models/Favourite";
 
-export const addFavourite = async (req: Request, res: Response, next: NextFunction) => {
-    try{
-        const {id, name, timestamp} = req.body;
+export const getFavourites = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const favourites = await Favourite.findAll();
+    res.status(200).json(favourites);
+  } catch (error) {
+    next(error);
+  }
+};
 
-        if(!id || !name || !timestamp){
-            return res.status(400).json({error: "Missing required fields."})
-        }
+export const addFavourite = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { id, name, timestamp } = req.body;
 
-        const exists = await Favourite.findByPk(id);
-        if(exists)[
-            return res.status()
-        ]
+    if (!id || !name || !timestamp) {
+      return res.status(400).json({ error: "Missing required fields." });
     }
-}
+
+    const exists = await Favourite.findByPk(id);
+    if (exists) {
+      return res.status(404).json({ error: "Favourite already exists." });
+    }
+    const favourites = await Favourite.create({ id, name, timestamp });
+    res.status(201).json(favourites);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteFavourite = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { id } = req.params;
+    const deleted = await Favourite.destroy({ where: { id } });
+
+    if (!deleted) {
+      return res.status(404).json({ error: "Favourite not found." });
+    }
+
+    res.status(201).json({ message: "Deleted successfully." });
+  } catch (error) {
+    next(error);
+  }
+};
