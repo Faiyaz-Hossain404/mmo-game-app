7452cfbfebd48548577a4d1fa346de9e6563d3f5
Fiayz <sifathhossain115@gmail.com>
2025-07-16 20:55:38 +0600

    Initialized controller

--- src/App.tsx
+++ src/App.tsx
@@ -26,3 +26,25 @@ function App() {
 }
 
 export default App;
+// import "./App.css";
+// import Home from "./Pages/Home";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import ShareState from "./Sharing/ShareState";
+// import FavouritesPage from "./Pages/FavouritesPage";
+// import GameDetailPage from "./Pages/GameDetailPage";
+
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<ShareState />}>
+//           <Route index element={<Home />} />
+//           <Route path="favourites" element={<FavouritesPage />} />
+//           <Route path="game/:id" element={<GameDetailPage />} />
+//         </Route>
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
